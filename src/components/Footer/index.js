d8ac6833bf19a71e94eb8bdc05a3a43cb29cdaa2
MCannon33 <marialcannon@gmail.com>
2021-03-27 01:38:19 -0400

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,35 @@
 import React from "react";
+import linkone from "../../assets/linkone.png";
+import githubone from "../../assets/githubone.png";
 
 function Footer() {
-  return <div className="footer" style={{ height: 60, width: "100%" }}></div>;
+  return (
+    <div className="footer">
+      <div class="servone">
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/marialcannon/">
+              <img
+                src={linkone}
+                className="my-2"
+                style={{ width: "10%" }}
+                alt="linkedlin"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/MCannon33">
+              <img
+                src={githubone}
+                className="my-2"
+                style={{ width: "10%" }}
+                alt="github"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 export default Footer;
